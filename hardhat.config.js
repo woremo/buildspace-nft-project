2f7bc01c2e2f6ab3351d84505311cb945164d0b1
Widi Oremo <widiochieng@gmail.com>
2022-03-14 07:51:21 +0300

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,8 +20,8 @@ module.exports = {
   solidity: "0.8.1",
   networks:{
     rinkeby:{
-      url:"",
-      accounts:[""]
+      url:"https://eth-rinkeby.alchemyapi.io/v2/2NUQND7ico1FOy52DwsrOHApwYS8V1v7",
+      accounts:["7d8a0987e89400082f4685c284065164c58a7d8369f8e8fcbdedad2c93582b6a"]
     }
   }
 };

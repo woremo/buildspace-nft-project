ec0e36ab96cd03ec6bc3c00517986a5290147958
Widi Oremo <widiochieng@gmail.com>
2022-03-14 07:56:14 +0300

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
 
 // // This is a sample Hardhat task. To learn how to create your own go to
 // // https://hardhat.org/guides/create-task.html
@@ -20,8 +21,8 @@ module.exports = {
   solidity: "0.8.1",
   networks:{
     rinkeby:{
-      url:"https://eth-rinkeby.alchemyapi.io/v2/2NUQND7ico1FOy52DwsrOHApwYS8V1v7",
-      accounts:["7d8a0987e89400082f4685c284065164c58a7d8369f8e8fcbdedad2c93582b6a"]
+      url:process.env.ALCHEMY_URI,
+      accounts:[ process.env.PRIVATE_KEY]
     }
   }
 };

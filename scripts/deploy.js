6124b00cca445f71c50e8976cb3c74f4067f79f4
Widi Oremo <widiochieng@gmail.com>
2022-03-15 08:47:02 +0300

    random collection

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,10 +10,17 @@ const main = async () => {
     await txn.wait()
     console.log("Minted NFT #1")
   
-    // txn = await nftContract.makeAnEpicNFT()
-    // // Wait for it to be mined.
-    // await txn.wait()
-    // console.log("Minted NFT #2")
+    txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+    console.log("Minted NFT #2")
+
+    txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+    console.log("Minted NFT #3")
+
+
   };
   
   const runMain = async () => {
